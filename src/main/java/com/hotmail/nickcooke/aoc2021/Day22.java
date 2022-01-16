e0e303e01db6e23974a894fa3c1ed7a5945ea85d
Nick Cooke <ncooke@ahnet.co.uk>
2022-01-16 22:23:25 +0000

    Day 22

--- src/main/java/com/hotmail/nickcooke/aoc2021/Day22.java
+++ src/main/java/com/hotmail/nickcooke/aoc2021/Day22.java
@@ -1,6 +1,5 @@
 package com.hotmail.nickcooke.aoc2021;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -13,46 +12,15 @@ import java.util.regex.Pattern;
 
 public class Day22 extends AoCSolution {
     
-    protected void part1() {
-        Set<Cube> onCubes = new HashSet<>();
-        Pattern reactorSwitchPattern = Pattern.compile( "(on|off) x=(-?\\d*)\\.\\.(-?\\d*),y=(-?\\d*)\\.\\.(-?\\d*),z=(-?\\d*)\\.\\.(-?\\d*)" );
-        for ( String input : inputLines ) {
-            Matcher reactorSwitchMatcher = reactorSwitchPattern.matcher( input );
-            reactorSwitchMatcher.find();
-            //            for ( int x =  Integer.parseInt( reactorSwitchMatcher.group( 2 )  ); x <=  Integer.parseInt( reactorSwitchMatcher.group( 3 ) ) ; x++ ) {
-            //                for ( int y =  Integer.parseInt( reactorSwitchMatcher.group( 4 )  ); y <=  Integer.parseInt( reactorSwitchMatcher.group( 5 ) ) ; y++ ) {
-            //                    for ( int z =  Integer.parseInt( reactorSwitchMatcher.group( 6 )  ); z <=  Integer.parseInt( reactorSwitchMatcher.group( 7 ) ) ; z++ ) {
-            for ( int x = Math.max( -50, Integer.parseInt( reactorSwitchMatcher.group( 2 ) ) ); x <= Math.min( 50, Integer.parseInt( reactorSwitchMatcher.group( 3 ) ) ); x++ ) {
-                for ( int y = Math.max( -50, Integer.parseInt( reactorSwitchMatcher.group( 4 ) ) ); y <= Math.min( 50, Integer.parseInt( reactorSwitchMatcher.group( 5 ) ) ); y++ ) {
-                    for ( int z = Math.max( -50, Integer.parseInt( reactorSwitchMatcher.group( 6 ) ) ); z <= Math.min( 50, Integer.parseInt( reactorSwitchMatcher.group( 7 ) ) ); z++ ) {
-                        Cube cube = new Cube( x, y, z );
-                        if ( "on".equals( reactorSwitchMatcher.group( 1 ) ) ) {
-                            onCubes.add( cube );
-                        }
-                        else {
-                            onCubes.remove( cube );
-                        }
-                    }
-                }
-            }
-        }
-        System.out.println( "Part 1: " + onCubes.size() );
-    }
-    
-    protected void part2() throws IOException {
+    protected void solve() {
         Set<Cuboid> cuboids = new HashSet<>();
         Pattern reactorSwitchPattern = Pattern.compile( "(on|off) x=(-?\\d*)\\.\\.(-?\\d*),y=(-?\\d*)\\.\\.(-?\\d*),z=(-?\\d*)\\.\\.(-?\\d*)" );
         for ( String input : inputLines ) {
-//            System.out.println( "Processing new cuboid: " + input );
             Matcher reactorSwitchMatcher = reactorSwitchPattern.matcher( input );
             reactorSwitchMatcher.find();
             Vertex pointFrom = new Vertex( Integer.parseInt( reactorSwitchMatcher.group( 2 ) ), Integer.parseInt( reactorSwitchMatcher.group( 4 ) ), Integer.parseInt( reactorSwitchMatcher.group( 6 ) ) );
-            Vertex pointTo = new Vertex( ( Integer.parseInt( reactorSwitchMatcher.group( 3 ) ) +1 ), ( Integer.parseInt( reactorSwitchMatcher.group( 5 ) ) +1), ( Integer.parseInt( reactorSwitchMatcher.group( 7 ) ) +1) );
+            Vertex pointTo = new Vertex( ( Integer.parseInt( reactorSwitchMatcher.group( 3 ) ) + 1 ), ( Integer.parseInt( reactorSwitchMatcher.group( 5 ) ) + 1 ), ( Integer.parseInt( reactorSwitchMatcher.group( 7 ) ) + 1 ) );
             Cuboid cuboid = new Cuboid( pointFrom, pointTo );
-            
-            if (pointFrom.z == 73728){
-                System.out.println("here!!!!!!!!!!!!!");
-            }
             
             if ( reactorSwitchMatcher.group( 1 ).equals( "on" ) ) {
                 cuboid.isOn = true;
@@ -62,49 +30,26 @@ public class Day22 extends AoCSolution {
             }
             else {
                 Set<Cuboid> combinedCuboids = new HashSet<>();
-                boolean addedNewCuboid = false;
                 for ( Cuboid existingCuboid : cuboids ) {
-//                    if ( existingCuboid.overlaps( cuboid ) ) {
-                        combinedCuboids.addAll( existingCuboid.combine( cuboid ) );
-//                        addedNewCuboid = true;
-//                    }
-//                    else {
-//                        combinedCuboids.add( existingCuboid );
-//                    }
+                    combinedCuboids.addAll( existingCuboid.combine( cuboid ) );
                 }
                 cuboids = combinedCuboids;
             }
         }
-        System.out.println( cuboids.size() );
         long initializationOnCubes = 0;
         long totalOnCubes = 0;
         for ( Cuboid cuboid : cuboids ) {
             totalOnCubes += cuboid.volume();
-            if (totalOnCubes < 0){
-                throw new RuntimeException();
-            }
             initializationOnCubes += cuboid.initializationVolume();
         }
-//        System.out.println( "initialization on: " + initializationOnCubes );
-        System.out.println( "total : " + totalOnCubes );
-    
-//        System.out.println(Cuboid.maxResult);
-//        System.out.println(Cuboid.minResult);
-    
-        Reactor reactor = new Reactor();
-        reactor.reboot();
-        
-        
-        //problem starts at line: 229
-        //on x=-17242..-13743,y=19611..29304,z=73728..76397
-        
+        System.out.println( "Part 1 : " + initializationOnCubes );
+        System.out.println( "Part 2 : " + totalOnCubes );
     }
     
-    public static void main( String[] args ) throws IOException {
+    public static void main( String[] args ) {
         Day22 day22 = new Day22();
         day22.getInput();
-        day22.part1();
-        day22.part2();
+        day22.solve();
     }
 }
 
@@ -119,64 +64,51 @@ class Cuboid {
     public String toString() {
         return "Volume: " + volume();
     }
+    
     public Cuboid( Vertex from, Vertex to ) {
-        if (from.x >= to.x || from.y>=to.y||from.z>=to.z){
-            throw new RuntimeException("All dimensions must be > 0, was " + from + "-> " + to);
-        }
         this.from = from;
         this.to = to;
     }
-    static long maxResult=Long.MIN_VALUE;
-    static long minResult = Long.MAX_VALUE;
+    
     public long volume() {
         long xLength = to.x - from.x;
         long yLength = to.y - from.y;
         long zLength = to.z - from.z;
-        long result =  xLength * yLength * zLength;
-        maxResult = Math.max(maxResult, result);
-        minResult = Math.min(minResult, result);
-        return result;
+        return xLength * yLength * zLength;
     }
     
     public long initializationVolume() {
-        if (to.x < -50 || from.x > 51 ||to.y < -50 || from.y > 51||to.z < -50 || from.z > 51)
-        {
+        if ( to.x < -50 || from.x > 51 || to.y < -50 || from.y > 51 || to.z < -50 || from.z > 51 ) {
             return 0;
         }
-        long xLength =(Math.min( 51, to.x ) - Math.max( -50, from.x ));
-        long yLength = (Math.min( 51, to.y ) - Math.max( -50, from.y ));
-        long zLength = (Math.min( 51, to.z ) - Math.max( -50, from.z ));
+        long xLength = ( Math.min( 51, to.x ) - Math.max( -50, from.x ) );
+        long yLength = ( Math.min( 51, to.y ) - Math.max( -50, from.y ) );
+        long zLength = ( Math.min( 51, to.z ) - Math.max( -50, from.z ) );
         return xLength * yLength * zLength;
     }
     
     protected boolean overlaps( Cuboid other ) {
-        return ( other.contains( this ) ||
-                  (( ( other.from.x >= this.from.x && other.from.x < this.to.x ) || ( this.from.x >= other.from.x && this.from.x < other.to.x ) )
-                && ( ( other.from.y >= this.from.y && other.from.y < this.to.y ) || ( this.from.y >= other.from.y && this.from.y < other.to.y ) )
-                && ( ( other.from.z >= this.from.z && other.from.z < this.to.z ) || ( this.from.z >= other.from.z && this.from.z < other.to.z ) ) ));
+        return ( other.contains( this ) || ( ( ( other.from.x >= this.from.x && other.from.x < this.to.x ) || ( this.from.x >= other.from.x && this.from.x < other.to.x ) ) && ( ( other.from.y >= this.from.y && other.from.y < this.to.y ) || ( this.from.y >= other.from.y && this.from.y < other.to.y ) ) && ( ( other.from.z >= this.from.z && other.from.z < this.to.z ) || ( this.from.z >= other.from.z && this.from.z < other.to.z ) ) ) );
     }
     
     public boolean contains( Cuboid other ) {
-        return ( other.from.x>= this.from.x && other.from.y >= this.from.y && other.from.z >= this.from.z ) && ( other.to.x <= this.to.x && other.to.y <= this.to.y && other.to.z <= this.to.z );
+        return ( other.from.x >= this.from.x && other.from.y >= this.from.y && other.from.z >= this.from.z ) && ( other.to.x <= this.to.x && other.to.y <= this.to.y && other.to.z <= this.to.z );
     }
     
     public Set<Cuboid> combine( Cuboid other ) {
         Set<Cuboid> combinedCuboids = new HashSet<>();
-        if (this.contains( other ) && other.isOn){
-            combinedCuboids.add(this);
-        }
-        else if (this.overlaps( other )) {
-            List<Integer> xs = new ArrayList<>( new HashSet<>( Arrays.asList( this.from.x, this.to.x , other.from.x, other.to.x ) ) );
+        if ( this.overlaps( other ) ) {
+            List<Integer> xs = new ArrayList<>( new HashSet<>( Arrays.asList( this.from.x, this.to.x, other.from.x, other.to.x ) ) );
             List<Integer> ys = new ArrayList<>( new HashSet<>( Arrays.asList( this.from.y, this.to.y, other.from.y, other.to.y ) ) );
             List<Integer> zs = new ArrayList<>( new HashSet<>( Arrays.asList( this.from.z, this.to.z, other.from.z, other.to.z ) ) );
             Collections.sort( xs );
             Collections.sort( ys );
             Collections.sort( zs );
             Set<Cuboid> subCuboids = new HashSet<>();
-            for ( int xIndex = 1; xIndex < xs.size() ; xIndex++ ) {
-                for ( int yIndex = 1; yIndex < ys.size() ; yIndex++ ) {
-                    for ( int zIndex = 1; zIndex < zs.size() ; zIndex++ ) {
-                        Cuboid newCuboid = new Cuboid( new Vertex( xs.get( xIndex -1 ), ys.get( yIndex -1), zs.get( zIndex-1)), new Vertex( xs.get( xIndex  ), ys.get( yIndex   ), zs.get( zIndex   ) ) );
+            for ( int xIndex = 1; xIndex < xs.size(); xIndex++ ) {
+                for ( int yIndex = 1; yIndex < ys.size(); yIndex++ ) {
+                    for ( int zIndex = 1; zIndex < zs.size(); zIndex++ ) {
+                        Cuboid newCuboid = new Cuboid( new Vertex( xs.get( xIndex - 1 ), ys.get( yIndex - 1 ), zs.get( zIndex - 1 ) ), new Vertex( xs.get( xIndex ), ys.get( yIndex ), zs.get( zIndex ) ) );
                         subCuboids.add( newCuboid );
                     }
                 }
@@ -185,23 +117,15 @@ class Cuboid {
                 if ( this.contains( candidate ) && !other.contains( candidate ) ) {
                     combinedCuboids.add( candidate );
                 }
-//                else if (other.isOn && other.contains( candidate )){
-//                    combinedCuboids.add( candidate );
-//
-//                }
             }
-            if ( other.isOn ) { //then add in the new cube if it's turned on
-                combinedCuboids.add( other );
-            }
-            
         }
         else {
             combinedCuboids.add( this );
-            if (other.isOn){
-                combinedCuboids.add( other );
-            }
         }
-        
+    
+        if ( other.isOn ) {
+            combinedCuboids.add( other );
+        }
         return combinedCuboids;
     }
     
@@ -257,41 +181,5 @@ class Vertex {
     @Override
     public String toString() {
         return "Vertex{" + "x=" + x + ", y=" + y + ", z=" + z + '}';
-    }
-}
-class Cube {
-    
-    int x;
-    
-    int y;
-    
-    int z;
-    
-    public Cube( int x, int y, int z ) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-    
-    @Override
-    public boolean equals( Object o ) {
-        if ( this == o ) {
-            return true;
-        }
-        if ( o == null || getClass() != o.getClass() ) {
-            return false;
-        }
-        Cube cube = (Cube) o;
-        return x == cube.x && y == cube.y && z == cube.z;
-    }
-    
-    @Override
-    public int hashCode() {
-        return Objects.hash( x, y, z );
-    }
-    
-    @Override
-    public String toString() {
-        return "Cube{" + "x=" + x + ", y=" + y + ", z=" + z + '}';
     }
 }
--- src/test/java/com/hotmail/nickcooke/aoc2021/CuboidTest.java
+++ src/test/java/com/hotmail/nickcooke/aoc2021/CuboidTest.java
@@ -2,6 +2,7 @@ package com.hotmail.nickcooke.aoc2021;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -63,6 +64,7 @@ class CuboidTest {
         assertTrue( cuboid1.contains(cuboid2) );
         assertFalse( cuboid2.contains(cuboid1) );
     }
+    
     @Test
     public void testVolume() {
         Cuboid cuboid = new Cuboid( new Vertex( 1, 1, 1 ), new Vertex( 2, 2, 2 ) );
@@ -83,16 +85,51 @@ class CuboidTest {
         cuboid = new Cuboid( new Vertex( -1, -1, -1 ), new Vertex( 1,1,1 ) );
         assertEquals( 8, cuboid.volume() );
     }
-//
+    
     @Test
     public void testCombines() {
     
-        Cuboid cuboid1 = new Cuboid( new Vertex( 1, 1, 1 ), new Vertex( 2, 2, 2 ) );
-        Cuboid cuboid2 = new Cuboid( new Vertex( 2, 2, 2 ), new Vertex( 3, 3, 3 ) );
-        cuboid2.isOn=true;
+        Cuboid cuboid1 = new Cuboid( new Vertex( 2, 2, 2 ), new Vertex(4,4,4 ) );
+        Cuboid cuboid2 = new Cuboid( new Vertex( 1, 1, 1 ), new Vertex( 5, 5, 5 ) );
         Set<Cuboid> combined = cuboid1.combine( cuboid2 );
-        assertEquals( 2, combined.size() );
+        assertEquals( 0, combined.size() );
         long volume= combined.stream().collect( Collectors.summingLong(Cuboid::volume) );
+        assertEquals( 0,volume );
+        cuboid2.isOn=true;
+         combined = cuboid1.combine( cuboid2 );
+        assertEquals( 1, combined.size() );
+        volume= combined.stream().collect( Collectors.summingLong(Cuboid::volume) );
+        assertEquals( 64,volume );
+    
+        cuboid1 = new Cuboid( new Vertex( 1, 1, 1 ), new Vertex( 5, 5, 5 ) );
+        cuboid2 = new Cuboid( new Vertex( 2, 2, 2 ), new Vertex(4,4,4 ) );
+        combined = cuboid1.combine( cuboid2 );
+        assertEquals( 26, combined.size() );
+        volume= combined.stream().collect( Collectors.summingLong(Cuboid::volume) );
+        assertEquals( 56,volume );
+    
+        Set<Cuboid> combined2 = new HashSet<>();
+        cuboid1.isOn=true;
+        for (Cuboid subCuboid : combined){
+            combined2.addAll( subCuboid.combine( cuboid1 ) );
+        }
+        volume= combined2.stream().collect( Collectors.summingLong(Cuboid::volume) );
+        assertEquals( 64,volume );
+    
+        combined2 = new HashSet<>();
+        cuboid2.isOn=true;
+        for (Cuboid subCuboid : combined){
+            combined2.addAll( subCuboid.combine( cuboid2 ) );
+        }
+        volume= combined2.stream().collect( Collectors.summingLong(Cuboid::volume) );
+        assertEquals( 64,volume );
+    
+        cuboid1 = new Cuboid( new Vertex( 1, 1, 1 ), new Vertex( 2, 2, 2 ) );
+        cuboid2 = new Cuboid( new Vertex( 2, 2, 2 ), new Vertex( 3, 3, 3 ) );
+        cuboid2.isOn=true;
+        combined = cuboid1.combine( cuboid2 );
+        assertEquals( 2, combined.size() );
+        volume= combined.stream().collect( Collectors.summingLong(Cuboid::volume) );
         assertEquals( 2,volume );
 
         cuboid1 = new Cuboid( new Vertex( 1, 1, 1 ), new Vertex( 3, 3, 3 ) );
@@ -145,31 +182,38 @@ class CuboidTest {
         volume= combined.stream().collect( Collectors.summingLong(Cuboid::volume) );
         assertEquals( 120,volume );
     }
-//
-//    @Test
-//    public void currentTest() {
-//
-//        Cuboid cuboid1 = new Cuboid( new Cube( 1, 1, 1 ), new Cube( 3, 3, 3 ) );
-//        //        cuboid2 = new Cuboid( new Cube( 1, 1, 1 ), new Cube( 3, 3, 3 ) );
-//        Cuboid cuboid2 = new Cuboid( new Cube( 3, 3, 3 ), new Cube( 3, 3, 3) );
-//        cuboid2.isOn=true;
-//        Set<Cuboid> combined = cuboid1.combine( cuboid2 );
-//        long volume= combined.stream().collect( Collectors.summingLong(Cuboid::volume) );
-//        assertEquals( 27,volume );
-//
-//
-//        cuboid1 = new Cuboid( new Cube( 1, 1, 1 ), new Cube( 3, 3, 3 ) );
-//        cuboid2 = new Cuboid( new Cube( 3,3,3 ), new Cube( 3,3,4 ) );
-//        cuboid2.isOn=true;
-//        combined = cuboid1.combine( cuboid2 );
-//        volume= combined.stream().collect( Collectors.summingLong(Cuboid::volume) );
-//        assertEquals( 28,volume );
-//
-//        cuboid1 = new Cuboid( new Cube( 1, 1, 1 ), new Cube( 3, 3, 3 ) );
-//        cuboid2 = new Cuboid( new Cube( 4,4,4 ), new Cube( 7,7,7 ) );
-//        cuboid2.isOn=true;
-//        combined = cuboid1.combine( cuboid2 );
-//        volume= combined.stream().collect( Collectors.summingLong(Cuboid::volume) );
-//        assertEquals( 91,volume );
-//    }
+    
+    @Test
+    public void moreTests() {
+    
+        Cuboid cuboid1 = new Cuboid( new Vertex( 1, 1, 1 ), new Vertex( 3, 3, 3 ) );
+        Cuboid cuboid2 = new Cuboid( new Vertex( 2, 2, 2 ), new Vertex( 5, 5, 5 ) );
+        Cuboid cuboid3 = new Cuboid( new Vertex( 0,0,0 ), new Vertex( 4,4,4) );
+        
+        cuboid1.isOn=true;
+        cuboid2.isOn=true;
+        cuboid3.isOn=false;
+        
+        Set<Cuboid> firstCombine = cuboid1.combine( cuboid2 );
+        Set<Cuboid> secondCombine = new HashSet<>();
+        for (Cuboid cuboid : firstCombine) {
+            secondCombine.addAll( cuboid.combine( cuboid3 ) );
+        }
+        assertEquals( 19, secondCombine.stream().collect( Collectors.summingLong(Cuboid::volume) ) );
+    
+        cuboid1 = new Cuboid( new Vertex( 0,0,0 ), new Vertex( 10,10,2) );
+        cuboid2 = new Cuboid( new Vertex( 0,0,6 ), new Vertex( 10,10,8 ) );
+        cuboid3 = new Cuboid( new Vertex( 0,0,0 ), new Vertex( 10,10,1) );
+    
+        cuboid1.isOn=true;
+        cuboid2.isOn=true;
+        cuboid3.isOn=true;
+    
+        firstCombine = cuboid1.combine( cuboid2 );
+        secondCombine = new HashSet<>();
+        for (Cuboid cuboid : firstCombine) {
+            secondCombine.addAll( cuboid.combine( cuboid3 ) );
+        }
+        assertEquals( 400, secondCombine.stream().collect( Collectors.summingLong(Cuboid::volume) ) );
+    }
 }
